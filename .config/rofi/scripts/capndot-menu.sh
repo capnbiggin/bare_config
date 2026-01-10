f9ac0a2ccb20cd511c5412d1d752275f7b2efe5b
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-01-10 00:01:19 -0600

    Initial Commit

---
+++ .config/rofi/scripts/capndot-menu.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Set Actual Home
+ACTUAL_USER="${SUDO_USER:-$USER}"
+ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
+
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/capndot/scripts
+ROFI_DIR="$ACTUAL_HOME"/.config/rofi
+
+menu() {
+  echo -e "$2" | rofi -dmenu -p "$1…"
+}
+
+terminal() {
+  ghostty --class=capndot.menu -e bash -c "$1"
+}
+
+browser() {
+  setsid google-chrome-stable --new-window --app="$1"
+}
+
+remove_menu() {
+  case $(menu "Remove" "󰣇  Package\n  Web App") in
+  *Package*) terminal "$SCRIPTS_DIR"/pkg-remove.sh ;;
+  *Web*) terminal "$SCRIPTS_DIR"/webapp-remove.sh ;;
+  *) main_menu ;;
+  esac
+}
+
+install_menu() {
+  case $(menu "Install" "󰣇  Arch Install\n󰣇  AUR Install\n  Web App") in
+  *Arch*) terminal "$SCRIPTS_DIR"/pkg-install.sh ;;
+  *AUR*) terminal "$SCRIPTS_DIR"/aur-pkg-install.sh ;;
+  *Web*) terminal "$SCRIPTS_DIR"/webapp-install.sh ;;
+  *) main_menu ;;
+  esac
+}
+
+learn_menu() {
+  case $(menu "Learn" " Keybindings\n  Hyprland\n󰣇  Arch\n  Neovim\n󱆃  Bash") in
+  *Keybindings*) "$SCRIPTS_DIR"/rofi-scripts/hypr-keybindings.sh ;;
+  *Hyprland*) browser "https://wiki.hypr.land/" & ;;
+  *Arch*) browser "https://wiki.archlinux.org/title/Main_page" & ;;
+  *Bash*) browser "https://devhints.io/bash" & ;;
+  *Neovim*) browser "https://www.lazyvim.org/keymaps" & ;;
+  *) main_menu ;;
+  esac
+}
+
+system_menu() {
+  case $(menu "System" "  Lock\n󰤄  Suspend\n󰜉  Restart\n󰐥  Shutdown") in
+  *Lock*) hyprlock ;;
+  *Suspend*) systemctl suspend ;;
+  *Restart*) systemctl reboot ;;
+  *Shutdown*) systemctl poweroff ;;
+  *) main_menu ;;
+  esac
+}
+
+main_menu() {
+  case $(menu "Start" " Apps\n  Tmux_Sessions\n  Project\n  Kill_Tmux_Session\n󰉉  Install\n󰉉  Uninstall\n󰧑  Learn\n  System\n- About") in
+  *Apps*) "$ROFI_DIR"/launchers/launcher-1.sh ;;
+  *Tmux_Sessions*) "$ROFI_DIR"/scripts/tmux_sessions.sh ;;
+  *Kill_Tmux_Session*) "$ROFI_DIR"/scripts/kill_tmux_sessions.sh ;;
+  *Project*) "$ROFI_DIR"/scripts/projects_menu.sh ;;
+  *Install*) install_menu ;;
+  *Uninstall*) remove_menu ;;
+  *Learn*) learn_menu ;;
+  *System*) system_menu ;;
+  *About*) ghostty --class=capndot.menu --title=About -e bash -c 'fastfetch; read -n 1 -s' ;;
+  esac
+}
+
+main_menu
---
+++ .config/shell/aliases.sh
@@ -0,0 +1,94 @@
+###################################
+#            Aliases              #
+###################################
+
+# File Aliases
+if [[ -x $(command -v eza) ]]; then
+  alias ls='eza -al --color=always --group-directories-first --icons=always'  # my preferred listing
+  alias la='eza -a --color=always --group-directories-first --icons=always'   # all files and dirs
+  alias ll='eza -lah --color=always --group-directories-first --icons=always' # long format
+  alias lt='eza -aT --color=always --group-directories-first --icons=always'  # tree listing
+fi
+
+# Change Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias cdot='cd ~/projects/capndot && printf " \U000F17A9 " && pwd'
+alias brain='cd ~/projects/My-Brain && printf " \U000F17A9 " && pwd'
+alias cd="zd"
+zd() {
+  if [ $# -eq 0 ]; then
+    builtin cd ~ && return
+  elif [ -d "$1" ]; then
+    builtin cd "$1"
+  else
+    z "$@" && printf " \U000F17A9 " && pwd || echo "Error: Directory not found"
+  fi
+}
+
+# File system
+alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
+
+# Alias to mod commands
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -rf'
+alias mkdir='mkdir -p'
+alias ping='ping -c 5'
+
+# pacman
+alias i='sudo pacman -S --noconfirm --needed'       # Install packages
+alias yayi='yay -S --noconfirm --needed'            # Install packages
+alias s='yay -Ss'                                   # Search packages
+alias Syy='sudo pacman -Syy --noconfirm --needed'   # update pacman & packages
+alias Syyu='sudo pacman -Syyu --noconfirm --needed' # update pacman & packages
+alias ySyu='yay -Syyu --noconfirm --needed'         # update pacman & packages
+alias ySyy='yay -Syy --noconfirm --needed'          # update pacman & packages
+
+# Editor
+alias n='nvim'
+alias sn='sudoedit'
+alias t='tmux'
+alias n.='nvim .'
+
+# Mis. Aliases
+alias c='clear'
+alias cf='clear && fastfetch'
+alias x='exit'
+alias szsh='source ~/.config/zsh/.zshrc'
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias stat='git status' # 'status' is protected name so using 'stat' instead
+alias tag='git tag'
+alias newtag='git tag -a'
+
+alias linutil='curl -fsSL https://christitus.com/linux | sh'
+
+# bare gti repo alias for managing dotfiles
+alias config="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+# -------------------------------------------
+#  Suffix Aliases - Open Files by Extension
+# -------------------------------------------
+# Just type the filename to open it with the associated program
+alias -s py='$EDITOR'
+alias -s md='$EDITOR'
+
+# --------------------------------------------
+# Chezmoi edit and apply
+# --------------------------------------------
+ce() {
+  $EDITOR $1
+  chezmoi add $1
+}
