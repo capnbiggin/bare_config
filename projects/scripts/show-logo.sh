23e1a9ac4d4655f4b62688a9c3ecab45d1976574
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-01-24 15:29:20 -0600

    Modified show-logo.sh

--- projects/scripts/show-logo.sh
+++ projects/scripts/show-logo.sh
@@ -5,7 +5,7 @@ ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
 
 # Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/.dotfiles/scripts
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
 
 source "$SCRIPTS_DIR"/script-beginer.sh
 
