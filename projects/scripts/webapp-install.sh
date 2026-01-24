6cc8f72170d55be23a81f30071268ff386a86688
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-01-24 15:29:52 -0600

    Modified webapp-install.sh

--- projects/scripts/webapp-install.sh
+++ projects/scripts/webapp-install.sh
@@ -5,7 +5,7 @@ ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
 
 # Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/.dotfiles/scripts
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
 
 if [ "$#" -ne 3 ]; then
   source "$SCRIPTS_DIR"/show-logo.sh
