bfedce2d2e02718fc36a5c7e400ecd869e4c32e7
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-01-24 15:41:46 -0600

    Modified niri.sh

--- projects/scripts/install/niri.sh
+++ projects/scripts/install/niri.sh
@@ -5,7 +5,7 @@ ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
 
 # Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/projects/capndot/scripts
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
 # Install Directory
 INSTALL_DIR="$SCRIPTS_DIR"/install
 
