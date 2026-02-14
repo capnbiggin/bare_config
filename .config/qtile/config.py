58c29c906d1d62bf819a9053c2adc29318238f04
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-14 00:20:37 -0600

    Modified config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -218,7 +218,7 @@ idle_inhibitors = []  # type: list
 @hook.subscribe.startup_once
 def start_once():
     home = os.path.expanduser("~")
-    subprocess.call([home + ".config/qtile/autostart.sh"])
+    subprocess.call([home + ".config/qtile/autostart_x11.sh"])
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
