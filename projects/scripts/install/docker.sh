cb40bd948ac869201be4de8211c341d945b2ac55
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-15 19:04:18 -0600

    Modified docker.sh

--- projects/scripts/install/docker.sh
+++ projects/scripts/install/docker.sh
@@ -5,9 +5,9 @@ ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
 
 # Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/projects/capndot/scripts
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
 
-source "$SCRIPTS_DIR/script-beginer.sh"
+source "$SCRIPTS_DIR/lib/common.sh"
 
 PACKAGES=(
   docker
