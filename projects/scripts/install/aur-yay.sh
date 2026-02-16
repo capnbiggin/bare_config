7caf8f55e4e11012c6f2f8e84cf1d2f0cee8a56d
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-15 18:57:27 -0600

    Modified aur-yay.sh

--- projects/scripts/install/aur-yay.sh
+++ projects/scripts/install/aur-yay.sh
@@ -5,11 +5,11 @@ ACTUAL_USER="${SUDO_USER:-$USER}"
 ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
 
 # Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/projects/capndot/scripts
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
 # Install Directory
 INSTALL_DIR="$SCRIPTS_DIR"/install
 
-source "$SCRIPTS_DIR"/script-beginer.sh
+source "$SCRIPTS_DIR"/lib/common.sh
 
 # Install yay AUR helper if not present
 if ! command -v yay &>/dev/null; then
