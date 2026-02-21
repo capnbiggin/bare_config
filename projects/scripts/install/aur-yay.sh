417c8cc65d2fc20ac267bcb1fbf35f42978d9d97
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-21 14:53:43 -0600

    Modified aur-yay.sh - Sat Feb 21 02:53:43 PM CST 2026

--- projects/scripts/install/aur-yay.sh
+++ projects/scripts/install/aur-yay.sh
@@ -13,22 +13,23 @@ source "$SCRIPTS_DIR"/lib/common.sh
 
 # Install yay AUR helper if not present
 if ! command -v yay &>/dev/null; then
-  log-info "\nInstalling yay AUR helper...\n"
+  log_info "\nInstalling yay AUR helper...\n"
   sudo pacman -S --needed git base-devel --noconfirm
   if [[ ! -d "yay" ]]; then
-    log-info "\nCloning yay repository...\n"
+    log_info "\nCloning yay repository...\n"
   else
-    log-info "\nyay directory already exists, removing it...\n"
+    log_info "\nyay directory already exists, removing it...\n"
     rm -rf yay
   fi
 
   git clone https://aur.archlinux.org/yay.git
 
   cd yay
-  log-info "\nBuilding yay.... yaaaaayyyyy\n"
+  log_info "\nBuilding yay.... yaaaaayyyyy\n"
   makepkg -si --noconfirm
   cd ..
   rm -rf yay
 else
-  log-info "$\nyay is already installed\n"
+  log_info "$\nyay is already installed\n"
 fi
+
