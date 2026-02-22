f9744df7a694ea5a8fc6a40ed27a5b22812f629e
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-21 19:14:10 -0600

    Modified hypr.sh - Sat Feb 21 07:14:10 PM CST 2026

---
+++ projects/scripts/install/hypr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Set Actual Home
+ACTUAL_USER="${SUDO_USER:-$USER}"
+ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
+
+# Script Directory
+SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
+# Install Directory
+INSTALL_DIR="$SCRIPTS_DIR"/install
+
+source "$SCRIPTS_DIR"/lib/common.sh
+source "$INSTALL_DIR"/packages.conf
+
+for pak in "${HYPR[@]}"; do
+  log_info "Installing $pak"
+  yay -S --needed --noconfirm "$pak"
+done
+systemctl --user add-wants niri.service
