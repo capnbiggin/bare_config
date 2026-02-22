1ecdf42e41b78e59063f137918f6f1cdf4aef07b
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-21 20:41:40 -0600

    Modified install - Sat Feb 21 08:41:40 PM CST 2026

---
+++ projects/scripts/install/configs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Set Actual Home
+ACTUAL_USER="${SUDO_USER:-$USER}"
+ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
+
+# Script Directory
+SCRIPTS_DIR="${ACTUAL_HOME}/projects/scripts"
+LIB_DIR="${SCRIPTS_DIR}/lib"
+# Install Directory
+INSTALL_DIR="${SCRIPTS_DIR}/install"
+
+source "${LIB_DIR}/common.sh"
+
+# Install git 
+sudo pacman -S git --needed --noconfirm
+
+git clone --bare https://github.com/capnbiggin/bare_config.git $HOME/.dotfiles
+
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+config checkout --force
+
+config config --local status.showUntrackedFiles no
