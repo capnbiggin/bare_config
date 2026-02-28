fed6cfee969821b755932d2d14c0ff480e56bfae
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-27 22:22:02 -0600

    Modified show-logo.sh - Fri Feb 27 10:22:02 PM CST 2026

--- projects/scripts/show-logo.sh
+++ projects/scripts/show-logo.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# Set Actual Home
-ACTUAL_USER="${SUDO_USER:-$USER}"
-ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
-
-# Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
-
-source "$SCRIPTS_DIR"/script-beginer.sh
+source ./lib/colors.sh
+source ./lib/common.sh
+source ./script-dir.sh
 
 clear
 echo -e "${CYAN}"
