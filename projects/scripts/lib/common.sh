9a424475a19a9eedd83b72216ca5afa52856fd93
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-27 22:06:32 -0600

    Modified common.sh - Fri Feb 27 10:06:32 PM CST 2026

--- projects/scripts/lib/common.sh
+++ projects/scripts/lib/common.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[96m'
-NC='\033[0m' # No Color
+source ./colors.sh
 
 # Logging functions
 log_info() {
