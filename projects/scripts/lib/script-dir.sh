fb914df67d411cd0c15f97ca31687462993a595c
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-27 22:10:35 -0600

    Modified script-dir.sh - Fri Feb 27 10:10:35 PM CST 2026

--- projects/scripts/lib/script-dir.sh
+++ projects/scripts/lib/script-dir.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# Set Actual Home
+ACTUAL_USER="${SUDO_USER:-$USER}"
+ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
+
+# Script Directory
+SCRIPTS_DIR="${ACTUAL_HOME}/projects/scripts"
 
