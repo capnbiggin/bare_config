90016594defb1c2c5d150abc8432da6aa89c65d1
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-28 00:55:43 -0600

    Modified webapp-remove.sh - Sat Feb 28 12:55:43 AM CST 2026

--- projects/scripts/webapp-remove.sh
+++ projects/scripts/webapp-remove.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# Set Actual Home
-ACTUAL_USER="${SUDO_USER:-$USER}"
-ACTUAL_HOME=$(eval echo "~$ACTUAL_USER")
-
-# Script Directory
-SCRIPTS_DIR="$ACTUAL_HOME"/projects/scripts
+source "${HOME}"/projects/scripts/lib/env.sh
+source "${LIB_DIR}"/colors.sh
+source "${LIB_DIR}"/lib/common.sh
 
 ICON_DIR="$ACTUAL_HOME/.local/share/applications/icons"
 DESKTOP_DIR="$ACTUAL_HOME/.local/share/applications/"
