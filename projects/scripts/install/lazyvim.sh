50cb6b332a17abd87246c88fe48fe0f6fbf4d1cd
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-28 00:20:20 -0600

    Modified install/lazyvim.sh - Sat Feb 28 12:20:20 AM CST 2026

--- projects/scripts/install/lazyvim.sh
+++ projects/scripts/install/lazyvim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 source ."${SCRIPTS_DIR}"/lib/env.sh
-source ."${SCRIPTS_DIR}"/colors.sh
-source ."${SCRIPTS_DIR}"/common.sh
+source ."${SCRIPTS_DIR}"/lib/colors.sh
+source ."${SCRIPTS_DIR}"/lib/common.sh
 
 log-info "\nCloning LazyVim\n"
 git clone https://github.com/LazyVim/starter ~/.config/nvim
