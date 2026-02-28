4a6b20598ad6d826e765848f38fe0a9bc5b9018e
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-02-27 22:46:51 -0600

    Modified show-logo.sh - Fri Feb 27 10:46:51 PM CST 2026

--- projects/scripts/show-logo.sh
+++ projects/scripts/show-logo.sh
@@ -3,10 +3,8 @@
 source "$SCRIPTS_DIR"/lib/colors.sh
 source "$SCRIPTS_DIR"/lib/common.sh
 
-LIB_DIR="$SCRIPTS_DIR"/lib
-
 clear
 echo -e "${CYAN}"
-cat <"$LIB_DIR"/logo.txt
+cat <."$SCRIPTS_DIR"/lib/logo.txt
 echo -e "${NC}"
 echo
