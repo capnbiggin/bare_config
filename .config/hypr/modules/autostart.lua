ba4d5403db67862304266236b84811a3580946d8
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-06-11 18:51:06 -0500

    Modified .config/hypr/modules/autostart.lua - Thu Jun 11 06:51:06 PM CDT 2026

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -1,5 +1,5 @@
 hl.on("hyprland.start", function ()
-  hl.exec_cmd("waybar & swaync & awww-daemon")
+  hl.exec_cmd("qs & swaync & awww-daemon")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
   hl.exec_cmd("wl-clip-persist --clipboard regular")
