ac81a5f494072c3f7241e8161dd82766a9064a72
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-06-18 22:01:34 -0500

    Modified .config/hypr/modules/keybinds.lua - Thu Jun 18 10:01:34 PM CDT 2026

--- .config/hypr/modules/keybinds.lua
+++ .config/hypr/modules/keybinds.lua
@@ -10,6 +10,7 @@ local editor1       = "zeditor"
 local editor2       = "code"
 local menu          = "~/.config/rofi/launchers/launcher-1.sh"
 local capn_menu     = "~/.config/rofi/scripts/capn-menu.sh"
+local power_menu    = "~/.config/rofi/scripts/capn-menu.sh power"
 local wallpaper     = "~/.config/rofi/scripts/wallpapers.sh"
 local cliphist      = "cliphist list | rofi -dmenu | cliphist decode | wl-copy"
 local waybar_toggle = "~/.config/waybar/scripts/launcher.sh"
@@ -34,6 +35,7 @@ hl.bind(mod .. " + CTRL + W", hl.dsp.exec_cmd(wallpaper))
 -- Menu binds
 hl.bind(mod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(mod .. " + SHIFT + D", hl.dsp.exec_cmd(capn_menu))
+hl.bind(mod .. " + SHIFT + D", hl.dsp.exec_cmd(power_menu))
 hl.bind(mod .. " + C", hl.dsp.exec_cmd(cliphist))
 
 -- screencap
